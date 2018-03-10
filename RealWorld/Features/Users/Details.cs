c8054d54530d5fe84fc61c5b8a0d0c0d20015580
hgirish <girish@post.com>
2018-03-09 19:51:42 -0800

    User Detail and Edit

---
+++ RealWorld/Features/Users/Details.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using System.Net;
+using System.Threading;
+using RealWorld.Infrastructure;
+using RealWorld.Infrastructure.Errors;
+
+namespace RealWorld.Features.Users
+{
+    public class Details
+    {
+        public class Query : IRequest<UserEnvelope>
+        {
+            public string Username { get; set; }
+        }
+
+        public class QueryValidator : AbstractValidator<Query>
+        {
+            public QueryValidator()
+            {
+                RuleFor(x => x.Username).NotNull().NotEmpty();
+            }
+        }
+
+        public class QueryHandler : IRequestHandler<Query, UserEnvelope>
+        {
+            private readonly AppDbContext _context;
+            private readonly IMapper _mapper;
+
+            public QueryHandler(AppDbContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+            public async  Task<UserEnvelope> Handle(Query message, CancellationToken cancellationToken)
+            {
+                var person = await _context.Persons
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(x => x.Username == message.Username, cancellationToken);
+
+                if (person == null)
+                {
+                    throw new RestException(HttpStatusCode.NotFound);
+                }
+
+                return new UserEnvelope(_mapper.Map<Domain.Person, User>(person));
+            }
+        }
+
+    }
+}
---
+++ RealWorld/Features/Users/UserController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using RealWorld.Infrastructure;
+using RealWorld.Infrastructure.Security;
+
+namespace RealWorld.Features.Users
+{
+    [Route("user")]
+    [Authorize(AuthenticationSchemes =JwtIssuerOptions.Schemes)]
+    public class UserController : Controller
+    {
+        private readonly IMediator _mediator;
+        private readonly ICurrentUserAccessor _currentUserAccessor;
+
+        public UserController(IMediator mediator, ICurrentUserAccessor currentUserAccessor)
+        {
+            _mediator = mediator;
+            _currentUserAccessor = currentUserAccessor;
+        }
+        [HttpGet]
+        public async Task<UserEnvelope> GetCurrent()
+        {
+            return await _mediator.Send(new Details.Query()
+            {
+                Username = _currentUserAccessor.GetCurrentUsername()
+            });
+        }
+
+        [HttpPut]
+        public async Task<UserEnvelope> UpdateUser([FromBody]Edit.Command command)
+        {
+            return await _mediator.Send(command);
+        }
+
+    }
+}
