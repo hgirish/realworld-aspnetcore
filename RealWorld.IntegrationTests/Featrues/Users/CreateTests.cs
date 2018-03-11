9fa19b9af63d84464af685fd18dedfccb55a3ff4
hgirish <girish@post.com>
2018-03-10 16:54:48 -0800

    Added Integration Tests

---
+++ RealWorld.IntegrationTests/Featrues/Users/CreateTests.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealWorld.Features.Users;
+using RealWorld.Infrastructure.Security;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RealWorld.IntegrationTests.Featrues.Users
+{
+    public    class CreateTests  : SliceFixture
+    {
+        [Fact]
+        public async Task Expect_Create_UserAsync()
+        {
+            var command = new Create.Command
+            {
+                User = new Create.UserData
+                {
+                    Email = "email@example.com",
+                    Password = "password",
+                    Username = "username"
+                }
+            };
+
+            await SendAsync(command);
+
+            var created = await ExecuteDbContextAsync(db => db.Persons.Where(
+                d => d.Email == command.User.Email).SingleOrDefaultAsync());
+            Assert.NotNull(created);
+            Assert.Equal(created.Hash, new PasswordHasher().Hash("password", created.Salt));
+        }
+        [Fact]
+        public async Task Invalid_email_throws_Exception()
+        {
+            const string email = "invalidemail";
+            var command = new Create.Command
+            {
+                User = new Create.UserData
+                {
+                    Email = email,
+                    Password = "password",
+                    Username = "username"
+                }
+            };
+
+            var ex = await Assert.ThrowsAsync<FluentValidation.ValidationException>(
+                async () => await SendAsync(command));
+            Assert.Contains("'Email' is not a valid email", ex.Message);
+
+
+        }
+        [Fact]
+        public async Task empty_password_throws_Exception()
+        {
+            const string email = "email@example.com";
+
+            const string password = "";
+            var command = new Create.Command
+            {
+                User = new Create.UserData
+                {
+                    Email = email,
+                    Password = password,
+                    Username = "username"
+                }
+            };
+
+            var ex = await Assert.ThrowsAsync<FluentValidation.ValidationException>(
+                async () => await SendAsync(command));
+            Assert.Contains("'Password' should not be empty", ex.Message);
+        }
+        [Fact]
+        public async Task empty_username_throws_Exception()
+        {
+            const string email = "email@example.com";
+
+            const string password = "password";
+            const string username = "";
+            var command = new Create.Command
+            {
+                User = new Create.UserData
+                {
+                    Email = email,
+                    Password = password,
+                    Username = username
+                }
+            };
+
+            var ex = await Assert.ThrowsAsync<FluentValidation.ValidationException>(
+                async () => await SendAsync(command));
+            Assert.Contains("'Username' should not be empty", ex.Message);
+        }
+        [Fact]
+        public async Task empty_email_throws_Exception()
+        {
+            const string email = "";
+
+            const string password = "password";
+            const string username = "username";
+            var command = new Create.Command
+            {
+                User = new Create.UserData
+                {
+                    Email = email,
+                    Password = password,
+                    Username = username
+                }
+            };
+
+            var ex = await Assert.ThrowsAsync<FluentValidation.ValidationException>(
+                async () => await SendAsync(command));
+            Assert.Contains("'Email' should not be empty", ex.Message);
+        }
+    }
+}
---
+++ RealWorld.IntegrationTests/Featrues/Users/LoginTests.cs
@@ -0,0 +1,82 @@
+﻿using RealWorld.Domain;
+using RealWorld.Features.Users;
+using RealWorld.Infrastructure.Errors;
+using RealWorld.Infrastructure.Security;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RealWorld.IntegrationTests.Featrues.Users
+{
+    public class LoginTests : SliceFixture
+    {
+        [Fact]
+        public async Task Expect_Login()
+        {
+            var salt = Guid.NewGuid().ToByteArray();
+            const string password = "password";
+            const string email = "email@example.com";
+            const string username = "username";
+            var person = new Person
+            {
+                Username = username,
+                Email = email,
+                Hash = new PasswordHasher().Hash(password, salt),
+                Salt = salt
+            };
+
+            await InsertAsync(person);
+
+            var command = new Login.Command
+            {
+                User = new Login.UserData
+                {
+                    Email = email,
+                    Password = password
+                }
+            };
+
+            var user = await SendAsync(command);
+
+            Assert.NotNull(user?.User);
+            Assert.Equal(user.User.Email, command.User.Email);
+            Assert.Equal(username, user.User.Username);
+            Assert.NotNull(user.User.Token);
+            Console.WriteLine(user.User.Token);
+
+
+        }
+
+        [Fact]
+        public async Task  Expect_RestException_On_Invalid_Credentials()
+        {
+            var salt = Guid.NewGuid().ToByteArray();
+            const string password = "password";
+            const string email = "email@example.com";
+            const string username = "username";
+            var person = new Person
+            {
+                Username = username,
+                Email = email,
+                Hash = new PasswordHasher().Hash(password, salt),
+                Salt = salt
+            };
+
+            await InsertAsync(person);
+
+            var command = new Login.Command
+            {
+                User = new Login.UserData
+                {
+                    Email = email,
+                    Password = "wrong"
+                }
+            };
+
+            RestException ex = await Assert.ThrowsAsync<RestException>(async () => await SendAsync(command));
+            Assert.Equal(System.Net.HttpStatusCode.Unauthorized, ex.Code);
+        }
+
+        
+    }
+}
